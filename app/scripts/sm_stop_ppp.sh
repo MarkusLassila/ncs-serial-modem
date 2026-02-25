506da482f77d5bc30aab0790624887cd04163584
Seppo Takalo <seppo.takalo@nordicsemi.no>
2026-02-25 15:09:55 +0200

    app: script: Improve handling of trace stops
    CMUX Modem traces should be stopped before any attempt to close the
    CMUX pipes. Otherwise it might hang up the ldattach utility.
    
    Signed-off-by: Seppo Takalo <seppo.takalo@nordicsemi.no>

--- app/scripts/sm_stop_ppp.sh
+++ app/scripts/sm_stop_ppp.sh
@@ -8,23 +8,21 @@ PIDFILE="/var/run/nrf91-modem.pid"
 PPP_PIDFILE="/var/run/ppp-nrf91.pid"
 TRACE_PID_FILE="/var/run/nrf91-modem-trace.pid"
 
+# Stop trace collection
+if [ -f $TRACE_PID_FILE ]; then
+        echo "Stopping trace collection..."
+        start-stop-daemon --stop --pidfile $TRACE_PID_FILE --remove-pidfile --oknodo --retry 1
+fi
+
 # Request PPPD to terminate
 if [ -f $PPP_PIDFILE ]; then
         echo "Stopping PPP link..."
-        kill -SIGTERM $(head -1 <$PPP_PIDFILE)
+        start-stop-daemon --stop --pidfile $PPP_PIDFILE
 fi
-
 
 # Wait for Shutdown script to complete
 if [ -f $PIDFILE ]; then
         echo "Waiting for Shutdown script to complete..."
         timeout 12s tail --pid=$(head -1 <$PIDFILE) -f /dev/null \
         || echo "Timeout waiting for Shutdown script to stop"
-fi
-
-# Stop trace collection
-if [ -f $TRACE_PID_FILE ]; then
-        echo "Stopping trace collection..."
-        kill $(cat $TRACE_PID_FILE) 2>/dev/null || true
-        rm -f $TRACE_PID_FILE
 fi
